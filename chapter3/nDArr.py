a132685ee0dec4d7d61f469e55c14e1189791e6c
ming9oo <mj10050203@gmail.com>
2023-03-11 16:06:52 +0900

    다차원배열

---
+++ chapter3/nDArr.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#1차원배열
+A = np.array([1,2,3,4])
+print(np.ndim(A))
+print(A.shape)
+print(A.shape[0])
+print()
+
+#2차원배열
+B = np.array([[1,2],[3,4],[5,6]])
+print(np.ndim(B))
+print(B.shape)
