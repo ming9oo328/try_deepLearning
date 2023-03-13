33734c956d2336a09a55fc5a3c6cdf583393db85
ming9oo <mj10050203@gmail.com>
2023-03-13 18:40:59 +0900

    3차원 신경망 출력층

--- chapter3/3DNeuralNetwork.py
+++ chapter3/3DNeuralNetwork.py
@@ -26,3 +26,13 @@ print(B2.shape)
 
 A2 = np.dot(Z1,W2)+B2 #1층의 Z1이 입력이 된다
 Z2 = sigmoid(A2)
+
+# 2층 -> 3층 
+def identity_function(x):
+    return x
+
+W3 = np.array([[0.1,0.3],[0.2,0.4]])
+B3 = np.array([0.1,0.2])
+
+A3 = np.dot(Z2,W3)+B3
+Y = identity_function(A3)
